bfb8a4761b3442f3b166c9646becc764ad000b15
unknown <sadeq.s502@gmail.com>
2022-08-15 15:13:01 +0430

    created user

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ const prisma = new PrismaClient()
 
 async function main() {
     //prisma queries here
+    const user = await prisma.user.create({ data: { name: "Changiz" } })
+    console.log(user);
     
 }
 
